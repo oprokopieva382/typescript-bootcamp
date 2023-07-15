ebc5d0a1c292aaa11e2376f0597744f379df3a09
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-07-15 15:51:36 -0500

    Tuple types

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 /*
-? Creating own literal type with Unions
+? Tuple types
 NOTES:
-
+- just like Array type, but,
+- tuples know exactly how many items it contains
+- tuples know which types are at which positions
+- tuples don't have representation in Runtime
  */
 
+type CarDetailsTuple = [number, string, boolean];
+
+const myCar: CarDetailsTuple = [2023, "Tesla", true];
+
+function printCarDetails(myCar: CarDetailsTuple) {
+  let [model, make, sedan] = myCar;
+
+  //! Error - Tuple type 'CarDetailsTuple' of length '3' has no element at index '3'
+  //   let cost = myCar[3];
+}
+
+printCarDetails([2023, "Tesla", true]);
