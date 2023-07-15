ea22838b1fb9110f2729a69a990303f13edf0d74
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-07-15 15:07:47 -0500

    Special type | "never"

--- index.ts
+++ index.ts
@@ -34,7 +34,7 @@ carT.accelerate = () => {
 carT = "BMW";
 
 //Even assigning it to wrong type will not cause error
-const myCar: number = carT
+const myCar: number = carT;
 
 /*
 ? Special type | "unknown"
@@ -45,7 +45,7 @@ NOTES:
 let a: unknown = document.getElementById("my-element");
 //we can do whatever we want with any type
 function getLatestCarModelV1(models: any) {
-    return models[0]
+  return models[0];
 }
 //but we cannot do whatever we want with unknown type
 // function getLatestCarModelV2(models: unknown) {
@@ -56,15 +56,49 @@ function getLatestCarModelV1(models: any) {
 NOTES:
 - In JS a function that doesn't return any value will implicitly return the value undefined
 - "void" and "undefined" is not the same thing in TS
-- 
  */
 
 //undefined | JS
 // void type| TS
 function printCarNameV1(): void {
-    console.log("BMW")
+  console.log("BMW");
 }
 
 let result = function printCarNameV2(): void {
   console.log("BMW");
+};
+
+/*? Special type | "never"
+NOTES:
+- some function never return a value
+- if nothing ar left in union it is type "never"
+-if the function terminates itself - "never"
+-if the function throw error - "never"
+
+ */
+
+//this function throws an exception
+//hence it never returns a value
+function crashCar(model: number): never {
+  throw new Error("Car crashed");
+}
+
+//this function terminates the program
+//hence it never returns a value
+//sudo code
+// import {exit} from "process";
+//  function terminate(process: number): never {
+//   exit(process)
+// }
+
+
+//"never" is also appears when nothing is left in a union
+function printCarDetails(make: string | number) {
+  if (typeof make === "string") {
+    console.log(make.toLocaleUpperCase());
+  } else if (typeof make === "number") {
+    console.log(make++);
+  } else {
+    //here is type of make is "never"
+  }
 }
