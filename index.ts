efca15472804376f3e3acdccdf4818dd6806e543
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-07-15 15:28:44 -0500

    Creating own literal type with Unions

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 /*
-? Literal types
+? Example 1
+? Creating own literal type with Unions
 NOTES:
-
+- The type boolean itself is actually just an alias for the union true | false.
  */
 
+//Let's create own literal type that can take few strings
+let car: "Tesla" | "BMW" | "Audi" = "Tesla";
 
+// OK
+car = "Audi"
+
+//! Error - Type '"Ford"' is not assignable to type '"Tesla" | "BMW" | "Audi"'.
+//car = "Ford"
