317baeaf18aaa3b7c465a1e533444d774fffac66
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-07-15 15:59:55 -0500

    Tuple with Interfaces

--- index.ts
+++ index.ts
@@ -5,13 +5,16 @@ NOTES:
 - tuples know exactly how many items it contains
 - tuples know which types are at which positions
 - tuples don't have representation in Runtime
+
+USES: 
+- some APIs return arrays and the items could be have their own meaning. We can use Tuples
  */
 
-type CarDetailsTuple = [number, string, boolean];
+type CarDetailsTuple1 = [number, string, boolean];
 
-const myCar: CarDetailsTuple = [2023, "Tesla", true];
+const myCar: CarDetailsTuple1 = [2023, "Tesla", true];
 
-function printCarDetails(myCar: CarDetailsTuple) {
+function printCarDetails(myCar: CarDetailsTuple1) {
   let [model, make, sedan] = myCar;
 
   //! Error - Tuple type 'CarDetailsTuple' of length '3' has no element at index '3'
@@ -19,3 +22,22 @@ function printCarDetails(myCar: CarDetailsTuple) {
 }
 
 printCarDetails([2023, "Tesla", true]);
+
+
+/*
+? Tuple with Interfaces
+NOTES:
+- Another way to declare a Tuple type is with interface(with specialized properties)
+ */
+
+interface CarDetailsTuple {
+    length: 4;
+    0: number;
+    1: string;
+    2: boolean
+    3: "UA" | "US"
+
+
+}
+
+const myCar2: CarDetailsTuple = [2023, "BMW", true, "US"]
